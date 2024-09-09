f3d58c975c3218dc5c47e137a6cb47783ac451cf
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-09-09 14:52:03 +0200

    dm1q: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-S911N Script version: 3.0.0

---
+++ omni_dm1q.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from dm1q device
+$(call inherit-product, device/samsung/dm1q/device.mk)
+
+PRODUCT_DEVICE := dm1q
+PRODUCT_NAME := omni_dm1q
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-S911N
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="dm1qksx-user 13 TP1A.220624.014 S911NKSS4CXGJ release-keys"
+
+BUILD_FINGERPRINT := samsung/dm1qksx/dm1q:13/TP1A.220624.014/S911NKSS4CXGJ:user/release-keys
